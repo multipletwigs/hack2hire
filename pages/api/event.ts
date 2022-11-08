fcf1fc0d066936d8c0827eb8e59a290e99ccc331
JH <44129179+lee-jian-hui@users.noreply.github.com>
2022-11-08 10:21:55 +0800

    some boilerplate for the BE

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -6,13 +6,39 @@ import { prisma } from "../../lib/prisma";
 export default async function handler(req: NextApiRequest,res: NextApiResponse) {
   
   if (req.method == "GET"){
+    const event: any = req.body;
+
+    /*
+    model Event {
+      id                 Int                  @id @default(autoincrement())
+      name               String
+      startAt            DateTime
+      endAt              DateTime
+      location           String
+      description        String
+      createdAt          DateTime             @default(now())
+      updatedAt          DateTime             @updatedAt
+      creatorId          Int
+      User               User                 @relation(fields: [creatorId], references: [id])
+      Activity           Activity[]
+      NGOforEvent        NGOforEvent[]
+      UserAttendingEvent UserAttendingEvent[]
+    }
+    */
+
+    const response: Event = await prisma.event.create({
+      data:{
+        id: event.id;
+        id: event
+      }
+    })
 
   }
   else if (req.method == "POST"){
 
   }
   else if (req.method) {
-    
+
   }
 }
 
