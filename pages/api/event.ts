829a9f21524954e6003a7114295f87d5f22bd246
JH <44129179+lee-jian-hui@users.noreply.github.com>
2022-11-08 12:21:47 +0800

    bugs with BE post request api/event

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -22,7 +22,7 @@ model Event {
 */
      
 export default async function handler(req: NextApiRequest,res: NextApiResponse) {
-  if (req.method == "GET") {
+  if (req.method === "GET") {
     try {
       // res.status(405).json({message: "get method under development" });
       const response: Event[] = await prisma.event.findMany();
@@ -34,7 +34,7 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
       res.status(405).json({message: "GET method error in event api", })
     }
   }
-  else if (req.method == "POST"){
+  else if (req.method === "POST"){
     try {
       const body: any = req.body;
       const response: Event = await prisma.event.create({
@@ -45,20 +45,51 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
           location: body.location,
           description: body.description,
           creatorId: body.creatorId,
-          Activity: body.activities,
-          NGOforEvent: body.ngos,
-          UserAttendingEvent: body.attending_users
+          // Activity: body.Activity,
+          // NGOforEvent: body.NGOforEvent,
+          // UserAttendingEvent: body.UserAttendingEvent
+          Activity: {
+            create: body.Activity.map((activity: any) => {
+              return {
+                name: activity.name, 
+                description: activity.description
+              };
+            })
+          },
+          // NGOforEvent: {
+          //   create: body.NGOforEvent.map((ngo: any) => {
+          //     return {
+          //       name: ngo.name, 
+          //       description: ngo.description
+          //     };
+          //   })
+          // },
+          // UserAttendingEvent: {
+          //   create: body.UserAttendingEvent.map((userAttendingEvent: any) => {
+          //     return {
+          //       name: userAttendingEvent.name, 
+          //       description: userAttendingEvent.description
+          //     };
+          //   })
+          // },
+
         }
       })  
+      console.log("response at POST block in api/event");
+      console.log(response);
       res.status(200).json({message: "created an event successfully!"})
     }
     catch (e){
       res.status(405).json({message: "POST method error in event api", })
     }
   }
-  else{
-    res.status(405).json({message: "HTTP method is not allowed / not supported yet!" });
+  else if (req.method === "PUT"){
+    res.status(405).json({message: "PUT method still under development", })
   }
+  // else{
+  //   console.log("else block in api/event");
+  //   res.status(400).json({message: "HTTP method is not allowed / not supported yet!" });
+  // }
 }
 
 
--- pages/dummyevent.tsx
+++ pages/dummyevent.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@chakra-ui/button";
 import { useEffect } from "react"
 
 
@@ -23,33 +24,55 @@ const data_to_send = {
   ],
   UserAttendingEvent: [
 
-  ]
-}
+  ],
+};
 
-const fetching = async () => {
+// const fetching = async () => {
+//   // NOTE: change HTTP methods here
+//   const response = await fetch("/api/event", {
+//     method: "POST",
+//     headers:{
+//       "Content-type": "application/json",
+//     },
+//     body: JSON.stringify(data_to_send)
+//   });
+
+//   console.log(response);
+//   return response;
+// } 
+
+const addEvent = async () => {
+  console.log("before calling API");
+  // console.log(data_to_send);
+
   const response = await fetch("/api/event", {
-    // NOTE: change HTTP methods here
     method: "POST",
     headers:{
       "Content-type": "application/json",
     },
     body: JSON.stringify(data_to_send)
-  })
+  });
+
+  console.log("response at FE");
   console.log(response);
-  return response.json();
+  return response;
+  
+  // console.log("error at addEvent");
+
+  // console.log(e);
+
 } 
 
-
 const Test_event = () => {
-  useEffect(() => {
-    fetching()
-  })
+  // useEffect(() => {
+  //   fetching()
+  // })
 
   return (
     <div>
-      test event 
+      <Button onClick={() =>  addEvent()}>Add Event</Button>
     </div>
-  )
+  );
 }
 
 export default Test_event;
