933e57c80cc20edc6f85954b009e0b3a2658f1d6
Zach Khong <63769232+multipletwigs@users.noreply.github.com>
2022-11-08 10:32:42 +0800

    Added in basic layouts and Navbar buttons

---
+++ pages/event/index.tsx
@@ -0,0 +1,17 @@
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
+import { PageTransition } from "../../animations/PageTransition";
+import NavBar from "../../components/NavBar";
+import Container from "../../layouts/Container";
+
+const EventPage = (props: any) => {
+  return (
+    <Container>
+        <Box>
+            Events page
+        </Box>
+    </Container>
+  );
+};
+
+export default EventPage;
