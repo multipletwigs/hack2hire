b5a264689c23943ff25f1338f6294244c35fb72b
Zach Khong <63769232+multipletwigs@users.noreply.github.com>
2022-11-09 08:30:23 +0800

    checking server endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,10 @@ export const server = dev ? 'http://localhost:3000' : 'https://hack2hire.vercel.
 
 export default function Index() {
   const router = useRouter()
+
+  useEffect(()=>{
+    console.log(server)
+  })
   return (
     useEffect(() => {
       router.push("/login")
