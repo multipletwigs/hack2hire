87c86dfbbbda8e7d62b371064d9e61c90fde706c
Zach Khong <63769232+multipletwigs@users.noreply.github.com>
2023-01-02 22:59:08 +0800

    remodified base

--- layouts/Container.tsx
+++ layouts/Container.tsx
@@ -8,25 +8,19 @@ import NavBar from "../components/NavBar";
 const Container = (props: any) => {
   const { children } = props;
   return (
-    <>
-      <Head>
-        <title>Dell Hack2Hire</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Box as="div" maxW="100vw" minH="100vh" bgColor="#F5F6FB">
-        <NavBar />
-        <Flex
-          flexDir={"column"}
-          justifyContent={"center"}
-          w={{ base: "90%", sm: "90%", md: "70%" }}
-          mt="5" 
-          mx="auto"
-        >
-          <PageTransition>{children}</PageTransition>
-          <Footer />
-        </Flex>
-      </Box>
-    </>
+    <Box as="div" maxW="100vw" minH="100vh" bgColor="#F5F6FB">
+      <NavBar />
+      <Flex
+        flexDir={"column"}
+        justifyContent={"center"}
+        w={{ base: "90%", sm: "90%", md: "70%" }}
+        mt="5"
+        mx="auto"
+      >
+        <PageTransition>{children}</PageTransition>
+        <Footer />
+      </Flex>
+    </Box>
   );
 };
 
